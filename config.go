3c4ee1f869f289f058b4118105b9460a3225211e
Lev Vandi <Lev-person11@yandex.ru>
2024-11-11 22:18:55 +0300

    adding get from error function and default-value functions (#1)

--- config.go
+++ config.go
@@ -10,36 +10,3 @@ var SeverityArray [4]string = [4]string{
 	"error",
 	"fatal",
 }
-
-// Default options for .Error()
-//
-// Sets as showing:
-// Message
-// Code
-var DefaultOptions Options = Options{
-	ShowMessage: true,
-	ShowCode:    true,
-}
-
-// Default options for logger
-//
-// Sets as showing:
-// DoLog
-// Message
-// Code
-// Severity
-// Description
-// Cause
-var DefaultLoggerOptions LoggerOptions = LoggerOptions{
-	DoLog:           true,
-	ShowMessage:     true,
-	ShowCode:        true,
-	ShowSeverity:    true,
-	ShowDescription: true,
-	ShowCause:       true,
-}
-
-// An empty logger settings
-//
-// It makes everything off
-var EmptyLoggerOptions LoggerOptions = LoggerOptions{}
